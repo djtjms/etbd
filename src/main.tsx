6acb7020fae21d201ecef2a55d104493a629b2d7
djtjms <djtjms64@gmail.com>
2026-02-22 19:07:59 +0000

    Remove initial loader from index.html and main.tsx for cleaner rendering

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import "./index.css";
 
-// Remove initial loader immediately before React mounts
-const initialLoader = document.getElementById("initial-loader");
-if (initialLoader) {
-  initialLoader.remove();
-}
-
 const rootEl = document.getElementById("root")!;
 
 createRoot(rootEl).render(
